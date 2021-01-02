0033b49427109e86967654f8a71cc0843384b262
Hridoy Roy <15103182@iubat.edu>
2021-01-02 13:22:19 +0600

    Add teacher controller and try to add update and view teacher information

---
+++ DapperInMVCCore/Controllers/TeacherController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using DapperInMVCCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperInMVCCore.Controllers
+{
+    public class TeacherController : Controller
+    {
+        //Create Database Connection
+        string Connection = "Data Source=DESKTOP-RMRMV4N; Initial Catalog=Student; Integrated Security=True";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // View Teacher Information
+        public IActionResult ViewTeacher()
+        {
+            using (SqlConnection connection = new SqlConnection(Connection))
+            {
+                connection.Open();
+                var teacher = connection.Query<Teacher>("select  * from Teacher").ToList();
+                connection.Close();
+                return View(teacher); 
+            }
+        }
+
+        // Create Teacher
+        [HttpGet]
+        public IActionResult CreateTeacher()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTeacher(Teacher teacher)
+        {
+            if (teacher!=null)
+            {
+                using (SqlConnection connection=new SqlConnection(Connection))
+                {
+                    connection.Open();
+                    var command = "Insert Into Teacher (TeacherName,TeacherEmail,TeacherPhone) values ('"+teacher.TeacherName+"','"+teacher.TeacherEmail+"','"+teacher.TeacherPhone+"')";
+                    connection.Execute(command);
+                    connection.Close();
+                    return RedirectToAction("ViewTeacher");
+                }
+            }
+            return View(teacher);
+        }
+
+        //Edit Teacher Information
+        [HttpGet]
+        public IActionResult EditTeacher(int id)
+        {
+            using (SqlConnection connection=new SqlConnection(Connection))
+            {
+                connection.Open();
+                var editteacher = connection.QueryFirstOrDefault<Teacher>("select * from Teacher where TeacherId=" + id + "");
+                connection.Close();
+                return View(editteacher);
+            } 
+        }
+
+        public IActionResult EditTeacher(Teacher teacher)
+        {
+            if (teacher!=null)
+            {
+                using (SqlConnection connection=new SqlConnection(Connection))
+                {
+                    connection.Open();
+                    var editteacher= "Update Teacher set TeacherName='" + teacher.TeacherName + "',TeacherEmail='" + teacher.TeacherEmail + "',TeacherPhone='" + teacher.TeacherPhone + "' where TeacherId=" + teacher.TeacherId + ";";
+                    connection.Execute(editteacher);
+                    connection.Close();
+                    return RedirectToAction("ViewTeacher");
+                }
+            }
+            return View(teacher);
+        }
+
+
+
+
+    }
+}
---
+++ DapperInMVCCore/Models/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperInMVCCore.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string TeacherName { get; set; }
+        public string TeacherEmail { get; set; }
+        public string TeacherPhone { get; set; }
+
+
+
+    }
+}
